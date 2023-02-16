8eedf90ba49e28ef4a39e3362651af177ef84b63
Jannik Voss <vossjannik@users.noreply.github.com>
2023-02-16 12:42:46 +0000

    Implement patch for canAccess/isAccessible for Java 8

--- run/build_java.sh
+++ run/build_java.sh
@@ -3,6 +3,19 @@ set -e
 
 export PATH=$(pwd)/dist:$PATH
 
+# apply patches depending on the installed java version
+java_version=$(java -version 2>&1)
+echo "Java version: $java_version"
+if [[ "$java_version" == *" 1.8"* ]]; then
+    echo "Applying patch: we need to use isAccessible before Java 9"
+    sed -i '' -e 's/canAccess(null)/isAccessible()/' './src/test/java/de/beckhoff/jni/tcads/amsaddr/test/AmsAddrTest.java'
+    function cleanup {
+        echo "Removing patch: prefer the newer canAccess method instead of isAccessible"
+        sed -i '' -e 's/isAccessible()/canAccess(null)/' './src/test/java/de/beckhoff/jni/tcads/amsaddr/test/AmsAddrTest.java'
+    }
+    trap cleanup EXIT
+fi
+
 mvn spotbugs:check -f "pom.xml"
 
 # this call also runs all the tests and create coverage report
