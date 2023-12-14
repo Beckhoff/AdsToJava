b610a3133b3f3e0cfe79e4b8a59debbd402504f1
Jannik Voss <j.voss@beckhoff.com>
2023-12-14 10:24:29 +0100

    Add support for cmake generators 'Visual Studio 16 2019' and 'Visual Studio 15 2017'

--- run/build_cpp.bat
+++ run/build_cpp.bat
@@ -8,12 +8,31 @@ IF EXIST build_x64 (
     rmdir /q /s build_x64 || exit /b %errorlevel%
 )
 
-mkdir build_win32 || exit /b %errorlevel%
-cmake -Hcpp -Bbuild_win32 -G "Visual Studio 17 2022" -A Win32 || exit /b %errorlevel%
-cmake --build build_win32 --config Release --target ALL_BUILD -j 14|| exit /b %errorlevel%
+setlocal enabledelayedexpansion
 
-mkdir build_x64|| exit /b %errorlevel%
-cmake -Hcpp -Bbuild_x64 -G "Visual Studio 17 2022" -A x64 || exit /b %errorlevel%
-cmake --build build_x64 --config Release --target ALL_BUILD -j 14 || exit /b %errorlevel%
+set vs_versions="Visual Studio 17 2022" "Visual Studio 16 2019" "Visual Studio 15 2017" "Visual Studio 14 2015"
+set newest_available=
+for %%v in (%vs_versions%) do (
+    cmake --help | findstr /C:%%v > nul
+    if not errorlevel 1 (
+        set newest_available=%%v
+        goto version_found
+    )
+)
+:version_found
 
-CALL run\copy_to_dist.bat
+if not %newest_available% == "" (
+    echo Newest available Visual Studio generator: %newest_available%
+
+    mkdir build_win32 || exit /b %errorlevel%
+    cmake -Hcpp -Bbuild_win32 -G %newest_available% -A Win32 || exit /b %errorlevel%
+    cmake --build build_win32 --config Release --target ALL_BUILD -j 14|| exit /b %errorlevel%
+
+    mkdir build_x64|| exit /b %errorlevel%
+    cmake -Hcpp -Bbuild_x64 -G %newest_available% -A x64 || exit /b %errorlevel%
+    cmake --build build_x64 --config Release --target ALL_BUILD -j 14 || exit /b %errorlevel%
+
+    CALL run\copy_to_dist.bat
+) else (
+    echo No Visual Studio generator found.
+)
--- run/build_cpp.sh
+++ run/build_cpp.sh
@@ -12,13 +12,28 @@ if [ -d "./build_x64" ]; then
 fi
 
 if [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "msys" ]]; then
-    mkdir ./build_win32
-    cmake -H./cpp -B./build_win32 -G "Visual Studio 17 2022" -A Win32
-    cmake --build ./build_win32 --config Release --target ALL_BUILD -j 14
+    visual_studio_versions=("Visual Studio 17 2022" "Visual Studio 16 2019" "Visual Studio 15 2017")
+    newest_available=""
+    for version in "${visual_studio_versions[@]}"; do
+        if cmake --help | grep -q "$version"; then
+            newest_available="$version"
+            break
+        fi
+    done
 
-    mkdir ./build_x64
-    cmake -H./cpp -B./build_x64 -G "Visual Studio 17 2022" -A x64
-    cmake --build ./build_x64 --config Release --target ALL_BUILD -j 14
+    if [ -n "$newest_available" ]; then
+        echo "Newest available Visual Studio generator: $newest_available"
+
+        mkdir ./build_win32
+        cmake -H./cpp -B./build_win32 -G "$newest_available" -A Win32
+        cmake --build ./build_win32 --config Release --target ALL_BUILD -j 14
+
+        mkdir ./build_x64
+        cmake -H./cpp -B./build_x64 -G "$newest_available" -A x64
+        cmake --build ./build_x64 --config Release --target ALL_BUILD -j 14
+    else
+        echo "No Visual Studio generator found."
+    fi
 else
     mkdir ./build
     cmake -H./cpp -B./build -G "Ninja"
